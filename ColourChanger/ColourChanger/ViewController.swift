2639b42d2e881452c44dcdb0ff98a5d499030982
Matt & Geoff <>
2017-08-03 11:54:19 +1200

    Updated controls

--- ColourChanger/ColourChanger/ViewController.swift
+++ ColourChanger/ColourChanger/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textViewInfo: UITextView!
+    @IBOutlet weak var btnReset: UIButton!
+    @IBOutlet weak var btnInstructions: UIButton!
+    @IBOutlet weak var textError: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textError.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnReset(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+        textViewInfo.textColor = UIColor.black
+        textError.text = ""
+        
+    }
 
+    @IBAction func btnInstructions(_ sender: Any) {
+        textViewInfo.isHidden = !textViewInfo.isHidden
+    }
 
+    @IBAction func btnSetColour(_ sender: Any) {
+        
+    }
 }
 
