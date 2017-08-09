b956b4aa7bc1344200bc5f638c29f8a93f82bee6
Geoff - Mac PC <yaweignasia@gmail.com>
2017-08-09 22:15:39 +1200

    Added controls, modified some properties and added Action/Outlets to provide required functionality. Still need to modify font colours based on RGB to conform with best practice UI

--- ColourChanger/ColourChanger/ViewController.swift
+++ ColourChanger/ColourChanger/ViewController.swift
@@ -15,12 +15,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnInstructions: UIButton!
     @IBOutlet weak var textError: UILabel!
     
+    @IBOutlet weak var sldrRed: UISlider!
+    @IBOutlet weak var sldrBlue: UISlider!
+    @IBOutlet weak var sldrGreen: UISlider!
+    
+    @IBOutlet weak var lblRed: UILabel!
+    @IBOutlet weak var lblGreen: UILabel!
+    @IBOutlet weak var lblBlue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         textError.text = ""
+        textViewInfo.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +40,13 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.white
         textViewInfo.textColor = UIColor.black
         textError.text = ""
-        
+        sldrRed.value = 0
+        sldrGreen.value = 0
+        sldrBlue.value = 0
+        textViewInfo.isHidden = true
+        lblRed.text = "0"
+        lblGreen.text = "0"
+        lblBlue.text = "0"
     }
 
     @IBAction func btnInstructions(_ sender: Any) {
@@ -40,7 +54,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnSetColour(_ sender: Any) {
+        let redCol = CGFloat(round(sldrRed.value)/255)
+        let greenCol = CGFloat(round(sldrGreen.value)/255)
+        let blueCol = CGFloat(round(sldrBlue.value)/255)
         
+        view.backgroundColor = UIColor(red: redCol, green: greenCol, blue: blueCol, alpha: 1.0)
+        
+    }
+    @IBAction func slrdRedChanged(_ sender: UISlider) {
+        lblRed.text = String(Int(round(sldrRed.value)))
+    }
+    @IBAction func sldrGreenChanged(_ sender: UISlider) {
+        lblGreen.text = String(Int(round(sldrGreen.value)))
+    }
+    @IBAction func sldrBlueChanged(_ sender: UISlider) {
+        lblBlue.text = String(Int(round(sldrBlue.value)))
     }
 }
 
